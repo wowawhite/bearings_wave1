c55c1c24fcc7a566ab44de0af3728d08f9750307
wowawhite <trash_bin@gmx.de>
2022-10-09 15:36:31 +0200

    added base program structure relative path interaction missing: wavelet part

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import numpy as np
+import scipy.signal as sg
+import os.path as path
+import pyaudio
+import wave
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# os agnostic global relative path calculations
+ROOT_DIR = path.realpath(path.join(path.dirname(__file__), '..'))
+DB_DIR = path.join(ROOT_DIR,"Storage")
+SOUNDFILES_DIR = path.join(ROOT_DIR,"Storage","Soundfiles")
+
+def infoheader(void):
+    infnstring = "Program for continious wavelet calculation. Alpha version."
+
+    print(f', {infnstring}')
+
+# here we nee some sort of mic recording and storing sound sequences in a database as time series data
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def readfile():
+    filename_hardcoded = path.join(SOUNDFILES_DIR, "testfile.wav")
+
+    soundfile = open(filename_hardcoded)
+    return soundfile
+
+
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.cwt.html
+def wavelet_stuff(void):
+    # do python magic
+    sg.cwt()  # (data, wavelet, widths, dtype=None, **kwargs)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    infoheader()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
